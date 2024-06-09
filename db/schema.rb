4e04e147e39f01347dec6fafac8f726e3de4bfa2
SymTr <sayumi.tsuruno@gmail.com>
2024-06-09 20:08:49 +0900

    devise set up "email removal"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_09_100047) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_09_105051) do
   create_table "posts", charset: "utf8", force: :cascade do |t|
     t.text "event", null: false
     t.text "self_task", null: false
@@ -20,6 +20,18 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_09_100047) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "emotions"
+  end
+
+  create_table "users", charset: "utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["nickname"], name: "index_users_on_nickname", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
