81255810bd7c610843ef14609a58bec8de8f8b0c
SymTr <sayumi.tsuruno@gmail.com>
2024-06-09 10:41:26 +0900

    Posts table creation

--- db/migrate/20240609005923_create_posts.rb
+++ db/migrate/20240609005923_create_posts.rb
@@ -1,7 +1,13 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-
+      t.text :event, null: false
+      t.text :emotions, null: false
+      t.text :self_task, null: false
+      t.text :other_task, null: false
+      t.text :mood_after, null: false
+      t.text :note
+      # t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
