38375d6a0a2c681a9b3b0e331d83a0bbecad5d79
SymTr <sayumi.tsuruno@gmail.com>
2024-06-09 19:12:18 +0900

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
   validates :event, :emotions, :self_task, :other_task, :mood_after, :note, presence: true
+ 
+  serialize :emotions, Array
 end
+
+# serializeメソッド
+# Rubyのオブジェクトをデータベースに保存する方法の一つ。配列やハッシュなどの複雑なオブジェクトを特定のテーブルのカラムに保存できる。
+# emotionsという名前のフィールド（または属性）をRubyのArrayとして扱うようRailsに指示している。
+# emotionsフィールドにデータを保存する際、Railsはそのデータを自動的にシリアル化（テキスト表現に変換）し、データベースに保存。
+# そのデータを取り出す時には、再びRubyのArrayオブジェクトにデシリアル化（テキスト表現からオブジェクトに変換）する。
