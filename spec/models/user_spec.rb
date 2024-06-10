c8adb776e6d97d8b4a612b93efaa501850d0aaba
SymTr <sayumi.tsuruno@gmail.com>
2024-06-10 17:44:39 +0900

    nickname email test with bot

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,13 +4,15 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     it 'nicknameが空では登録できない' do
-      user = User.new(nickname: '', email: 'test@example.com', password: 'password')
+      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user.nickname = ''  # nicknameの値を空にする
       user.valid?
       expect(user.errors.full_messages).to include("ニックネーム を入力してください")
     end
 
     it 'emailが空では登録できない' do
-      user = User.new(nickname: 'testuser', email: '', password: 'password')
+      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user.email = ''  # emailの値を空にする
       user.valid?
       expect(user.errors.full_messages).to include("メールアドレス を入力してください")
     end
