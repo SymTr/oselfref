98e7482c33ba605466fee6772c52429fcfbd81bc
SymTr <sayumi.tsuruno@gmail.com>
2024-06-10 15:35:52 +0900

    fix validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :event, :emotions, :self_task, :other_task, :mood_after, :note, presence: true
+  validates :event, :emotions, :self_task, :other_task, :mood_after, presence: true
  
   serialize :emotions, Array
   belongs_to :user
