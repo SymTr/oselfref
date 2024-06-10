7f1388ceb265808394f1d7bcd8665add91890593
SymTr <sayumi.tsuruno@gmail.com>
2024-06-10 17:47:50 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,19 +2,21 @@ require 'rails_helper'
 # Rspecを用いてRailsの機能をテストするときに、共通の設定を書いておくファイル
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
   describe 'ユーザー新規登録' do
     it 'nicknameが空では登録できない' do
-      user = FactoryBot.build(:user)  # Userのインスタンス生成
-      user.nickname = ''  # nicknameの値を空にする
-      user.valid?
-      expect(user.errors.full_messages).to include("ニックネーム を入力してください")
+      @user.nickname = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("ニックネーム を入力してください")
     end
 
     it 'emailが空では登録できない' do
-      user = FactoryBot.build(:user)  # Userのインスタンス生成
-      user.email = ''  # emailの値を空にする
-      user.valid?
-      expect(user.errors.full_messages).to include("メールアドレス を入力してください")
+      @user.email = ''  # emailの値を空にする
+      @user.valid?
+      expect(@user.errors.full_messages).to include("メールアドレス を入力してください")
     end
   end
 end
