ea7b1c2d7257984ab570a24d29ae2111daa8a461
SymTr <sayumi.tsuruno@gmail.com>
2024-06-10 17:40:55 +0900

    test nickname email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
+# Rspecを用いてRailsの機能をテストするときに、共通の設定を書いておくファイル
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+      user = User.new(nickname: '', email: 'test@example.com', password: 'password')
+      user.valid?
+      expect(user.errors.full_messages).to include("ニックネーム を入力してください")
+    end
+
+    it 'emailが空では登録できない' do
+      user = User.new(nickname: 'testuser', email: '', password: 'password')
+      user.valid?
+      expect(user.errors.full_messages).to include("メールアドレス を入力してください")
+    end
+  end
 end
