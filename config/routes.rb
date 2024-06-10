cce2092f9bdd7059b5102ab73198bdceb3698e20
SymTr <sayumi.tsuruno@gmail.com>
2024-06-10 15:16:42 +0900

    homes controller set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'homes/index'
+  
   devise_for :users
 
   resources :posts, only: [:index, :new, :create]
 
   # Define root path
-  root to: 'posts#index'
+  root to: 'homes#index'
 end
