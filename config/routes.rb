255d5e1d53f397d519f2cc127708eae7ea9ac326
SymTr <sayumi.tsuruno@gmail.com>
2024-06-12 11:25:09 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'homes/index'
   
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 
   resources :posts, only: [:index, :new, :create]
 
