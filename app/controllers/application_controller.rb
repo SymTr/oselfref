bc039d00fea5a806549a4f0a5b8ef4ab39360e70
SymTr <sayumi.tsuruno@gmail.com>
2024-06-13 09:40:15 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 # app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2024'
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email])
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :email])
-  end
+  end 
 end
