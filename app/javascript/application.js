6606efa12db3ab7fd2916b713f19bccb95e42104
SymTr <sayumi.tsuruno@gmail.com>
2024-07-01 14:17:04 +0900

    Update application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,9 @@
+// app/javascript/application.js
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
 
-import { base64URLStringToBuffer, bufferToBase64URLString } from './webauthn_helper'
+import { base64URLStringToBuffer, bufferToBase64URLString } from 'helpers/webauthn_helper'
 
 window.base64URLStringToBuffer = base64URLStringToBuffer;
 window.bufferToBase64URLString = bufferToBase64URLString;
