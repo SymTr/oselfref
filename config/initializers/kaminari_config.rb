60c3b56bd31689c52abf546ed41ce941b03fe610
SymTr <sayumi.tsuruno@gmail.com>
2024-07-24 10:57:41 +0900

    Update kaminari_config.rb

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,8 +1,7 @@
 Kaminari.configure do |config|
   config.default_per_page = 10
-  config.max_per_page = 100
-  config.window = 4
-  config.outer_window = 0
-  config.left = 0
-  config.right = 0
+  config.window = 2
+  config.outer_window = 1
+  config.left = 1
+  config.right = 1
 end
