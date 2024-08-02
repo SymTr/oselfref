d74ac31d7c6ed7371b53fee299e7d2e5c0734644
SymTr <sayumi.tsuruno@gmail.com>
2024-08-02 11:54:40 +0900

    Update posts.rb

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,12 +1,16 @@
 # spec/factories/posts.rb
 FactoryBot.define do
   factory :post do
-    event { 'Some event' }
-    emotions { %w[喜び 悲しみ] }
-    self_task { 'Self task' }
-    other_task { 'Other task' }
-    mood_after { 'Mood after' }
-    note { 'Note' }
+    # eventの定義を削除または正しい属性名に修正
+    situation { "テスト状況" }
+    self_task { "自分のタスク" }
+    others_task { "他人のタスク" }
+    mood_after { "後の気分" }
+    alternative_thinking { "代替思考" }
+    emotions { ["喜び", "悲しみ"] }
+    thoughts { "思考" }
+    supporting_evidence { "支持する証拠" }
+    contrary_evidence { "反対の証拠" }
     association :user
   end
 end
